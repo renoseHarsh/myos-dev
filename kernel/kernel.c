db5e42b258771f42ed969193fd3738a9d7574710
renoseHarsh <renoseharsh@gmail.com>
2025-05-25 23:46:40 +0530

    Added scrolling

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -2,7 +2,14 @@
 #include <stdbool.h>
 int main()
 {
-    for (int i = 0; i < 26; i++) {
-        vga_putc('A' + i % 26);
+    int nex = 0;
+    while (true) {
+        for (int i = 0; i < 26; i++) {
+            vga_putc('A' + ((nex + i) % 26));
+        }
+        nex++;
+        vga_putc('\n');
+        if (nex % 2) vga_set_color(VGA_COLOR_GRAY, VGA_COLOR_BLACK);
+        else vga_set_color(VGA_COLOR_BLACK, VGA_COLOR_GRAY);
     }
 }
--- kernel/vga.c
+++ kernel/vga.c
@@ -3,6 +3,8 @@
 #include <stdint.h>
 
 #define VGA_MEMORY (uint16_t *)0xB8000
+#define columns 80
+#define rows 25
 
 static uint16_t vga_color = 7; // Grey on Black
 static uint16_t *const vga_buffer = VGA_MEMORY;
@@ -14,9 +16,22 @@ static void outb(uint16_t port, uint8_t data)
     __asm__("outb %0, %1" ::"a"(data), "d"(port));
 }
 
+static void vga_scroll()
+{
+    for (int crow = 1; crow < rows; crow++) {
+        for (int ccol = 0; ccol < columns; ccol++) {
+            vga_buffer[((crow - 1) * columns) + ccol]
+                = vga_buffer[(crow * columns) + ccol];
+        }
+    }
+    for (int ccol = 0; ccol < columns; ccol++) {
+        vga_buffer[((rows - 1) * columns) + ccol] = 0;
+    }
+}
+
 static void movCursor()
 {
-    uint16_t pos = cursor_row * 80 + cursor_col;
+    uint16_t pos = cursor_row * columns + cursor_col;
     outb(0x3D4, 0x0E);
     outb(0x3D5, pos >> 8);
     outb(0x3D4, 0x0F);
@@ -30,17 +45,21 @@ void vga_set_color(const enum vga_color bg, const enum vga_color fg)
 
 void vga_putc(const char c)
 {
+    if (cursor_row == rows) {
+        vga_scroll();
+        cursor_row = rows - 1;
+    }
     if (c == '\n') {
         cursor_row++;
         cursor_col = 0;
     } else {
-        vga_buffer[(cursor_row * 80) + cursor_col++] = (vga_color << 8) | c;
-        if (cursor_col == 80) {
+        vga_buffer[(cursor_row * columns) + cursor_col++]
+            = (vga_color << 8) | c;
+        if (cursor_col == columns) {
             cursor_row++;
             cursor_col = 0;
         }
     }
-    if (cursor_row == 25) cursor_row = 0;
     movCursor();
 }
 
